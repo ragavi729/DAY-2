da285e83bc063b02e9207ba82e9769b47023a928
RAGAVI <ragavik43@gmail.com>
2025-11-10 11:48:23 +0530

    Add files via upload

---
+++ day 2 pbslv Q10.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("%d\n%d\n", num1, num2);
+
+    return 0;
+}
