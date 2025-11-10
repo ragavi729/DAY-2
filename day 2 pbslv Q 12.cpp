49eca94f0134787c471013122fb96e5f29693c04
RAGAVI <ragavik43@gmail.com>
2025-11-10 11:57:27 +0530

    Add files via upload

---
+++ day 2 pbslv Q 12.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int sub1, sub2, sub3, sub4, sub5;
+    printf("Enter marks in 5 subjects: ");
+    scanf("%d %d %d %d %d", &sub1, &sub2, &sub3, &sub4, &sub5);
+    printf("Marks in Subject 1: %d\n", sub1);
+    printf("Marks in Subject 2: %d\n", sub2);
+    printf("Marks in Subject 3: %d\n", sub3);
+    printf("Marks in Subject 4: %d\n", sub4);
+    printf("Marks in Subject 5: %d\n", sub5);
+
+    return 0;
+}
